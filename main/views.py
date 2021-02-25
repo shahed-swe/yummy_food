e6a273bef449b6218a3f19aa5d8207d2f3c3ce25
Md Shahed Talukder <shahedtalukder51@gmail.com>
2021-02-25 11:23:34 +0600

    new commit

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world")
+    return render(request, 'front/user/home.html',{"title":"Home"})
